38c02805d11b377aeea12b6a4c7a82b2d04acd95
abmo0002 <abmo0002@stud.kea.dk>
2023-09-26 18:41:54 +0200

    Adventure

---
+++ src/main/java/org/example/UserInterface.java
@@ -0,0 +1,48 @@
+package org.example;
+
+import java.util.Scanner;
+
+public class UserInterface {
+
+
+    Adventure adventure = new Adventure();
+    Scanner scanner = new Scanner(System.in);
+    String input;
+
+    public UserInterface() {
+    }
+
+    public void startGame() {
+
+
+        adventure.buildMap();
+
+        System.out.println("\nWelcome to the adventure game!");
+        System.out.println("Write help for a list of instructions\n");
+
+        do {
+            System.out.print("You are in the following room: ");
+            System.out.println(adventure.getCurrentRoom());
+
+            System.out.println("\nWhere do you wish to go?");
+            input = scanner.nextLine();
+
+            switch (input.toLowerCase()) {
+                case "go north" -> adventure.moveAround(input);
+                case "go south" -> adventure.moveAround(input);
+                case "go east" -> adventure.moveAround(input);
+                case "go west" -> adventure.moveAround(input);
+                case "help" -> System.out.println("If you want to move North, write: \"go north\"\n" +
+                        "If you want to move East, then write \"go East\"\n" +
+                        "If you want to move South, then write \"go south\"\n" +
+                        "If you want to move West, then write \"go west\"\n" +
+                        "If you want to know what room you're in write: \"look around\"\n" +
+                        "If you wish to end the game write: \"exit\"");
+                case "look" -> System.out.println(adventure.getCurrentRoom().getDescription());
+                case "exit" -> System.out.println("Thank you for playing ");
+                default -> System.out.println("Not a known command!. Write help for instructions");
+            }
+        } while (!input.equalsIgnoreCase("exit"));
+    }
+}
+
